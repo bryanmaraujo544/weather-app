35d2205cb57a99d9388e1993f8f0ef1207cb3c10
Bryan Martins <bryanmaraujo544@gmail.com>
2022-01-24 17:07:03 -0300

    feat: adding the styles of the week weather data

--- src/components/OtherWeatherData/index.tsx
+++ src/components/OtherWeatherData/index.tsx
@@ -1,11 +1,27 @@
 import { Header } from '../Header';
-import { Container } from './styles';
+import { Container, WeekWeather } from './styles';
 
+import SunIcon from '../../assets/icons/sun/4.png';
+
+
+const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']; 
 
 export const OtherWeatherData = () => {
   return (
     <Container>
       <Header />
+      <WeekWeather>
+        {weekDays.map((day) => (
+          <div className="day-weather-card">
+            <div className="day">{day}</div>
+            <img src={SunIcon} alt="wather-icon" />
+            <div className="temperature-min-max">
+              <p className="max">15°</p>
+              <div className="min">3°</div>
+            </div>
+          </div>
+        ))}
+      </WeekWeather>
       'OtherWatherdata'
     </Container>
   )
--- src/components/OtherWeatherData/styles.ts
+++ src/components/OtherWeatherData/styles.ts
@@ -2,9 +2,59 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-start;
   align-items: center;
   flex-direction: column;
-  padding: 3.2rem;
+  padding: 3.2rem 4.8rem;
   background: ${({ theme }) => theme.colors.gray[100]};
+  gap: 3.2rem;
 `;  
+
+export const WeekWeather = styled.div`
+  display: flex;
+  width: 100%;
+  gap: .8rem;
+  margin-top: 1.6rem;
+
+  .day {
+    font-size: 1.2rem;
+    font-weight: 500;
+  }
+
+  .day-weather-card {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    gap: 1.2rem;
+    width: 100%;
+
+    border-radius: 24px;
+    padding: 1.4rem;
+    background: ${({ theme }) => theme.colors.white};
+    box-shadow: 0 1px 4px #00000005;
+
+    img {
+      height: 4.2rem;
+    }
+
+    .temperature-min-max {
+      display: flex;
+      gap: .6rem;
+  
+      .max, .min {
+        font-size: 1.2rem;
+        font-weight: 500;
+      }
+  
+      .max {
+        color: ${({ theme }) => theme.colors.gray[900]};
+      }
+  
+      .min {
+        color: ${({ theme }) => theme.colors.gray[300]};
+      }
+    }
+  }
+
+`;
